ca056c5bdb420e2bc91a8e77fe674a8a369d6da7
neelkaria <neelkaria.enkay@gmail.com>
2021-04-18 02:26:30 +0530

    Pause Menu and End Menu added

--- src/Elements/Menus.java
+++ src/Elements/Menus.java
@@ -5,19 +5,36 @@ import java.awt.event.*;
 
 public class Menus extends JFrame {
 	
-	public Menus(){
-
-	}
-	
 	public void endgame()
 	{
-	    JLabel l1;  
-	    l1=new JLabel("First Label.");  
-	    l1.setBounds(100,100, 100,30);  
+	    JLabel l1 = new JLabel(new ImageIcon("C:\\Users\\Neel\\OneDrive\\Desktop\\game over.jpg"));
+	    JButton b1 = new JButton(new ImageIcon("C:\\Users\\Neel\\OneDrive\\Desktop\\restart.png"));
+	    setLocation(550,300);
+	    setSize(512,425);
+	    getContentPane().setBackground(Color.BLACK);
+	    setLayout(null);
+	    setVisible(true);
+	    b1.setBounds(160,300,150,30);
+	    l1.setBounds(0,-50,500,400);
+	    add(b1);
 	    add(l1);
-	    setSize(300,300);  
-	    setLayout(null);  
-	    setVisible(true);  
+	    
+	}
+	
+	public void pausegame()
+	{
+		JLabel l1 = new JLabel(new ImageIcon("C:\\Users\\Neel\\OneDrive\\Desktop\\paused.jpg"));
+	    //JButton b1 = new JButton(new ImageIcon("C:\\Users\\Neel\\OneDrive\\Desktop\\resumegame.png"));
+	    setLocation(0,22);
+	    setSize(1600,900);
+	    setUndecorated(true);
+	    setOpacity(0.5f);
+	    setLayout(null);
+	    setVisible(true);
+	    l1.setBounds(0,0,1600,900);
+	    //b1.setBounds(620,600,350,100);
+	    //add(b1);
+	    add(l1);
 		
 	}
 }

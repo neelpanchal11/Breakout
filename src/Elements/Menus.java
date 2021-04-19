bc27e452f2a951df5369ea07a43f649c68db332a
neelkaria <neelkaria.enkay@gmail.com>
2021-04-19 14:16:12 +0530

    beautification, adjustments and photos changed

--- src/Elements/Menus.java
+++ src/Elements/Menus.java
@@ -11,35 +11,41 @@ public class Menus extends JFrame {
 	{
 	    JFrame f1 = new JFrame();
 	    JLabel l1;
+	    JLabel l2;
+	    
 	    if (won)
 	    {
 	    	l1 = new JLabel(new ImageIcon("sprites\\game over.jpeg"));
+	    	l2 = new JLabel();
 	    }else
 	    {
-	    	l1 = new JLabel(new ImageIcon("sprites\\me.jpeg"));
+	    	l1 = new JLabel(new ImageIcon("sprites\\me.jpg"));
+	    	l2 = new JLabel(new ImageIcon("sprites\\lose.jpg"));
 	    }
-	    JButton reset = new JButton(new ImageIcon("sprites\\restart.jpg"));
+	    JButton reset = new JButton(new ImageIcon("sprites\\reset.jpg"));
 	    JButton exit = new JButton(new ImageIcon("sprites\\exit.jpg"));
 	    
-	    reset.setBounds(300,300,150,30);
-	    exit.setBounds(325,350,100,35);
-	    l1.setBounds(90,-50,600,500);
-	    
+	    reset.setBounds(280,580,200,60);
+	    exit.setBounds(280,650,200,50);
+	    l1.setBounds(100,120,600,440);
+	    l2.setBounds(130,20,500,100);
 	    exit.addActionListener(new BL());
 	    
 	    f1.setUndecorated(true);
 	    f1.getContentPane().setBackground(Color.BLACK);
 	    f1.setLayout(null);
 	    f1.setVisible(true);
-	    f1.setBounds(430,250,800,500);
-	    
+	    f1.setBounds(430,100,800,750);
+		
 	    f1.add(reset);
 	    f1.add(exit);
 	    f1.add(l1);
-	    
+	    f1.add(l2);
+
 	    f1.setAlwaysOnTop(true);
 	}
-
+	
+	
 	public void pausegame(int w, int h)
 	{
 		JLabel l1 = new JLabel(new ImageIcon("sprites\\paused.jpeg"));

7ed56aa2601acfd133e51cb88fe51700234ee177
neelkaria <neelkaria.enkay@gmail.com>
2021-05-10 18:31:10 +0530

    level select matrix added

---
+++ src/Elements/level_select.java
@@ -0,0 +1,52 @@
+package Elements;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class level_select extends JPanel 
+{	
+	JButton lev_button_gen[][] = new JButton[3][3];
+	ImageIcon lev_arr[][] = {{new ImageIcon("sprites\\levels\\demo.png"), new ImageIcon("sprites\\levels\\lev1.png"), new ImageIcon("sprites\\levels\\lev2.png")},
+							 {new ImageIcon("sprites\\levels\\lev3.png"), new ImageIcon("sprites\\levels\\lev4.png"), new ImageIcon("sprites\\levels\\lev5.png")},
+							 {new ImageIcon("sprites\\levels\\lev6.png"), new ImageIcon("sprites\\levels\\lev6.png"), new ImageIcon("sprites\\levels\\lev6.png")}};
+	static int lev_no;
+	public level_select(int w, int h)
+	{	
+		this.setBounds(w/4,h/6,800,350);
+		this.setBackground(new Color(0,0,0,80));
+		this.setLayout(null);
+		gen_mat();
+	}
+	
+	public void gen_mat()
+	{
+		for( int i = 0; i < 3; i++)
+		{
+			for( int j = 0; j < 3; j++)
+			{
+				lev_button_gen[i][j] = new JButton(lev_arr[i][j]);
+				lev_button_gen[i][j].putClientProperty("column", i);
+				lev_button_gen[i][j].putClientProperty("row", j);
+				lev_button_gen[i][j].setBounds(10 + 280*i, 10 + 120*j, 220, 70);
+				lev_button_gen[i][j].addActionListener(new get_lev_no());
+				add(lev_button_gen[i][j]);
+			}
+		}
+	}
+	
+	public class get_lev_no implements ActionListener
+	{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			JButton btn = (JButton) e.getSource();
+	        lev_no = (int) btn.getClientProperty("row") + 3 * ((int) btn.getClientProperty("column"));
+	        System.out.println(lev_no);              
+		}	
+	}
+}
--- src/Elements/main_menu.java
+++ src/Elements/main_menu.java
@@ -22,60 +22,58 @@ public class main_menu extends JPanel
 	JFrame run;
 	public JButton exit;
 	public JButton start;
+	public JButton lev_button;
+	public JLabel title;
 	public bg_music bg;
-	public JComboBox<?> levelbox;
-	public int lev_no;
+	public level_select select;
+	//public JComboBox<?> levelbox;
 	public boolean on = false;
+	
 	public main_menu(int w, int h, JFrame run)
 	{
-			
-		ImageIcon lev_arr[] = {
-				new ImageIcon("sprites\\levels\\demo.png"),
-				new ImageIcon("sprites\\levels\\lev1.png"),
-				new ImageIcon("sprites\\levels\\lev2.png"),
-				new ImageIcon("sprites\\levels\\lev3.png"),
-				new ImageIcon("sprites\\levels\\lev4.png"),
-				new ImageIcon("sprites\\levels\\lev5.png"),
-				new ImageIcon("sprites\\levels\\lev6.png")
-				};	
 		
 		this.run = run;
 		exit = new JButton(new ImageIcon("sprites\\exit2.jpg"));
 		start = new JButton(new ImageIcon("sprites\\start_button.jpg"));
-		JLabel lev_label = new JLabel(new ImageIcon("sprites\\level.jpg"));	
-		JLabel title = new JLabel(new ImageIcon("sprites\\welcome.png"));
-		bg = new bg_music();
+		lev_button = new JButton(new ImageIcon("sprites\\level.jpg"));
+		//JLabel lev_label = new JLabel(new ImageIcon("sprites\\level.jpg"));	
+		title = new JLabel(new ImageIcon("sprites\\welcome.png"));
+		bg = new bg_music();	
+		//levelbox = new JComboBox(lev_arr);
 			
-		levelbox = new JComboBox(lev_arr);
-			
-		levelbox.setUI(new javax.swing.plaf.metal.MetalComboBoxUI()
-				{public void layoutComboBox(Container parent, MetalComboBoxLayoutManager manager) 
-					{super.layoutComboBox(parent, manager);
-					 arrowButton.setBounds(0,0,0,0);
-					 }});
+//		levelbox.setUI(new javax.swing.plaf.metal.MetalComboBoxUI()
+//				{public void layoutComboBox(Container parent, MetalComboBoxLayoutManager manager) 
+//					{super.layoutComboBox(parent, manager);
+//					 arrowButton.setBounds(0,0,0,0);
+//					 }});
 			
 		title.setBounds(180, 200, 1200, 400);
-		lev_label.setBounds(650,720,220,70);
-		lev_label.setVisible(true);
-			
+		lev_button.setBounds(650,720,220,70);
+		lev_button.setVisible(true);
+		lev_button.addActionListener(new lev_sel());
+		
 		start.setBounds(400,720,220,70);
 		bg.mute_button.setBounds(25*w/26,h/80,50,50);
-		levelbox.setBounds(650,720,220,70);
+		//levelbox.setBounds(650,720,220,70);
 		exit.setBounds(900,720,220,70);
-			
+		
+		
 		this.setBounds(0,0,w,h);
 		this.setLayout(null);
 		this.setBackground(new Color(50,50,50));
 		this.add(bg.mute_button);
 		this.add(title);
-		this.add(lev_label);
+		this.add(lev_button);
 		this.add(start);
-		this.add(levelbox);
+		//this.add(levelbox);
 		this.add(exit);
 			
-		
 		exit.addActionListener(new exit());	
 		bg.start();
+		
+		select = new level_select(w,h);
+		select.setVisible(false);
+		this.add(select);
 	}
 
 	
@@ -86,5 +84,20 @@ public class main_menu extends JPanel
 		{
 			System.exit(0);
 		}
+	}
+	
+	public class lev_sel implements ActionListener
+	{
+
+		@Override
+		public void actionPerformed(ActionEvent e) 
+		{
+			select.setVisible(true);
+			title.setVisible(false);
+			start.setVisible(false);
+			exit.setVisible(false);
+			lev_button.setVisible(false);
+		}
+		
 	}
 }
